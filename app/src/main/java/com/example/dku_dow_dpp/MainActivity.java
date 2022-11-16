f01a34b3c7a7b82379d79a24366eb971c255a947
Sumin Choi <meaning110v@gmail.com>
2022-11-16 14:52:37 +0900

    first commit

--- app/src/main/java/com/example/dku_dow_dpp/MainActivity.java
+++ app/src/main/java/com/example/dku_dow_dpp/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.dku_dow_dpp;
 
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,7 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // ?
     }
+
+    // ?
+
 }
