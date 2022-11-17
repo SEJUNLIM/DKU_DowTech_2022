b77acccf4a5fe9af9e5333d1f02c1e8a882a3c0d
Sumin Choi <meaning110v@gmail.com>
2022-11-17 16:40:50 +0900

    first commit

--- app/src/main/java/com/example/dku_dow_dpp/MainActivity.java
+++ app/src/main/java/com/example/dku_dow_dpp/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.dku_dow_dpp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageButton my_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        my_btn = findViewById(R.id.my_btn);
+        my_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MyPageActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
