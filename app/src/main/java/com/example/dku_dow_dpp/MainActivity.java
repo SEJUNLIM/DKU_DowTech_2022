cc2ccd0c52971bf730846a748ef839d0d44c30bc
Sumin Choi <meaning110v@gmail.com>
2022-11-17 20:09:18 +0900

    first commit

--- app/src/main/java/com/example/dku_dow_dpp/MainActivity.java
+++ app/src/main/java/com/example/dku_dow_dpp/MainActivity.java
@@ -1,31 +1,15 @@
 package com.example.dku_dow_dpp;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageButton;
-
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    private ImageButton my_btn;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        my_btn = findViewById(R.id.my_btn);
-        my_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, MyPageActivity.class);
-                startActivity(intent);
-            }
-        });
     }
 
 
