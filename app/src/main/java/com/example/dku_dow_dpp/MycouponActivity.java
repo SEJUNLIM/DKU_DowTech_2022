4880256becdfde5e1c3b12d99c14e9e29b04f925
SeJun Lim <lsj1137@naver.com>
2022-11-17 21:11:59 +0900

    마이쿠폰 버튼 활성화

--- app/src/main/java/com/example/dku_dow_dpp/MycouponActivity.java
+++ app/src/main/java/com/example/dku_dow_dpp/MycouponActivity.java
@@ -1,13 +1,29 @@
 package com.example.dku_dow_dpp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MycouponActivity  extends AppCompatActivity {
+
+    Button buyCoupon;
+    ImageButton backBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mycoupon);
+
+        buyCoupon = findViewById(R.id.buyCouponButton);
+        buyCoupon.setOnClickListener(view -> {
+            Intent intent = new Intent(MycouponActivity.this,BuyCouponActivity.class);
+            startActivity(intent);
+        });
+
+        backBtn = findViewById(R.id.backButton);
+        backBtn.setOnClickListener(view -> onBackPressed());
     }
 }
