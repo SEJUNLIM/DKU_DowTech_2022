e352b7505abe726901aa0716e4997f75d057d9a0
csb0710 <sdssond@gmail.com>
2022-11-17 19:52:02 +0900

    2022/11/17 1

--- app/src/main/java/com/example/dku_dow_dpp/MainActivity.java
+++ app/src/main/java/com/example/dku_dow_dpp/MainActivity.java
@@ -9,90 +9,10 @@ import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-    Button firstbtn;
-    Button firstMakeBtn;
-    Button secondbtn;
-    Button secondMakeBtn;
-    Button thirdbtn;
-    Button thirdMakeBtn;
-    EditText et_Text;
-    String food;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_babpick);
-
-        firstbtn = findViewById(R.id.first_btn);
-        firstbtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, SplashActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        firstMakeBtn = findViewById(R.id.first_btn2);
-        firstMakeBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, BabpickmakingActivity.class);
-
-                et_Text = findViewById(R.id.firstFood);
-                food = et_Text.getText().toString();
-
-                intent.putExtra("food", food);
-
-                startActivity(intent);
-            }
-        });
-
-        secondbtn = findViewById(R.id.second_btn);
-        secondbtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, SplashActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        secondMakeBtn = findViewById(R.id.second_btn2);
-        secondMakeBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, BabpickmakingActivity.class);
-
-                et_Text = findViewById(R.id.secondFood);
-                food = et_Text.getText().toString();
-
-                intent.putExtra("food", food);
-
-                startActivity(intent);
-            }
-        });
-
-        thirdbtn = findViewById(R.id.third_btn);
-        thirdbtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, SplashActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        thirdMakeBtn = findViewById(R.id.third_btn2);
-        thirdMakeBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, BabpickmakingActivity.class);
-
-                et_Text = findViewById(R.id.thirdFood);
-                food = et_Text.getText().toString();
-
-                intent.putExtra("food", food);
-
-                startActivity(intent);
-            }
-        });
     }
 }
