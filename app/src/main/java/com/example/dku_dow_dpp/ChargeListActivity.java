1036ea1c39b712c95b4fa0366e0971a532c1a4b0
Sumin Choi <meaning110v@gmail.com>
2022-11-18 23:08:48 +0900

    my page button

--- app/src/main/java/com/example/dku_dow_dpp/ChargeListActivity.java
+++ app/src/main/java/com/example/dku_dow_dpp/ChargeListActivity.java
@@ -1,19 +1,37 @@
 package com.example.dku_dow_dpp;
 
+import static android.content.ContentValues.TAG;
+
 import android.content.Intent;
 import android.graphics.Paint;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.Objects;
 
 public class ChargeListActivity extends AppCompatActivity {
 
     private Button point_charge, charge_list, page;
     private Button oneMonth, threeMonth, sixMonth, year;
     private ImageButton back_btn, my_btn;
+    private TextView charge_date, charge_money;
+    private FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +47,8 @@ public class ChargeListActivity extends AppCompatActivity {
         sixMonth = findViewById(R.id.six_month);
         year = findViewById(R.id.one_year);
         page = findViewById(R.id.page);
+        charge_date = findViewById(R.id.charge_date);
+        charge_money = findViewById(R.id.charge_money);
 
         charge_list.setPaintFlags(charge_list.getPaintFlags()|Paint.UNDERLINE_TEXT_FLAG);
         page.setPaintFlags(page.getPaintFlags()| Paint.UNDERLINE_TEXT_FLAG);
@@ -57,5 +77,40 @@ public class ChargeListActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        db = FirebaseFirestore.getInstance();
+        CollectionReference product = db.collection("user").document("lim").collection("pointcharge");
+        product.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    for (DocumentSnapshot doc : task.getResult()) {
+                        charge_date.setText(doc.getData().get("chargeDate").toString());
+                        charge_money.setText("₩".concat(doc.getData().get("chargePoint").toString()));
+                    }
+                } else {
+                    Log.d(TAG, "get failed with ", task.getException());
+                }
+            }
+        });
+        /*
+        db = FirebaseFirestore.getInstance();
+        DocumentReference productRef = db.collection("user").document("lim");
+        productRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        Log.d(TAG, "DocumentSnapshot data: " + document.getData());
+                    } else {
+                        Log.d(TAG, "No such document");
+                    }
+                } else {
+                    Log.d(TAG, "get failed with ", task.getException());
+                }
+            }
+        });
+        */
     }
 }
--- app/src/main/java/com/example/dku_dow_dpp/MainActivity.java
+++ app/src/main/java/com/example/dku_dow_dpp/MainActivity.java
@@ -1,29 +1,51 @@
 package com.example.dku_dow_dpp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 
 import android.content.Intent;
 import android.os.Bundle;
 
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 
+import com.google.android.material.navigation.NavigationBarView;
+
 public class MainActivity extends AppCompatActivity {
 
-    private ImageButton my_btn;
+    MainFragment mainFragment;
+    MyCouponFragment myCouponFragment;
+    BabpickFragment babpickFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        my_btn = findViewById(R.id.my_btn);
-        my_btn.setOnClickListener(new View.OnClickListener() {
+        mainFragment = new MainFragment();
+        myCouponFragment = new MyCouponFragment();
+        babpickFragment = new BabpickFragment();
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.containers, mainFragment).commit();
+
+        NavigationBarView navigationBarView = findViewById(R.id.bottom_navi);
+        navigationBarView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, MyPageActivity.class);
-                startActivity(intent);
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.main:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.containers, mainFragment).commit();
+                        return true;
+                    case R.id.coupon:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.containers, myCouponFragment).commit();
+                        return true;
+                    case R.id.babpick:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.containers, babpickFragment).commit();
+                        return true;
+                }
+                return false;
             }
         });
     }
