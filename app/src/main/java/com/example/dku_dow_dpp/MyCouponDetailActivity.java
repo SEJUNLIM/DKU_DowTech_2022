d5e7cf515f9715f4fc86a6920f6e6fde33672eae
SeJun Lim <lsj1137@naver.com>
2022-11-19 17:19:49 +0900

    마이쿠폰 버그 수정, 마이쿠폰 디테일 뒤로가기 추가, 프래그먼트 뒤로가기 삭제

--- app/src/main/java/com/example/dku_dow_dpp/MyCouponDetailActivity.java
+++ app/src/main/java/com/example/dku_dow_dpp/MyCouponDetailActivity.java
@@ -1,14 +1,19 @@
 package com.example.dku_dow_dpp;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MyCouponDetailActivity extends AppCompatActivity {
+    ImageButton backBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_coupon_detail);
 
+        backBtn = findViewById(R.id.backButton);
+        backBtn.setOnClickListener(view -> onBackPressed());
     }
 }
