f36a5e391f46710ced09f665c2885f4087c6b831
SeJun Lim <lsj1137@naver.com>
2022-11-19 15:04:18 +0900

    마이쿠폰 액티비티 완성

--- app/src/main/java/com/example/dku_dow_dpp/BuyCouponActivity.java
+++ app/src/main/java/com/example/dku_dow_dpp/BuyCouponActivity.java
@@ -24,6 +24,8 @@ public class BuyCouponActivity  extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buy_coupon);
+
+        intent = new Intent(BuyCouponActivity.this,CouponListActivity.class);
         restaurant = findViewById(R.id.restaurant);
         cafe = findViewById(R.id.cafe);
         convenient = findViewById(R.id.convenient);
@@ -32,70 +34,21 @@ public class BuyCouponActivity  extends AppCompatActivity {
         book = findViewById(R.id.book);
         internet = findViewById(R.id.internetLecture);
         culture = findViewById(R.id.culture);
-        intent = new Intent(BuyCouponActivity.this,CouponListActivity.class);
 
         backBtn = findViewById(R.id.backButton);
         backBtn.setOnClickListener(view -> onBackPressed());
-        restaurant.setOnClickListener(view -> {
-            categoryClick(0);
-            startActivity(intent);
-        });
-        cafe.setOnClickListener(view -> {
-            categoryClick(1);
-            startActivity(intent);
-        });
-        convenient.setOnClickListener(view -> {
-            categoryClick(2);
-            startActivity(intent);
-        });
-        office.setOnClickListener(view -> {
-            categoryClick(3);
-            startActivity(intent);
-        });
-        beauty.setOnClickListener(view -> {
-            categoryClick(4);
-            startActivity(intent);
-        });
-        book.setOnClickListener(view -> {
-            categoryClick(5);
-            startActivity(intent);
-        });
-        internet.setOnClickListener(view -> {
-            categoryClick(6);
-            startActivity(intent);
-        });
-        culture.setOnClickListener(view -> {
-            categoryClick(7);
-            startActivity(intent);
-        });
+        restaurant.setOnClickListener(view -> categoryClick(0));
+        cafe.setOnClickListener(view -> categoryClick(1));
+        convenient.setOnClickListener(view -> categoryClick(2));
+        office.setOnClickListener(view -> categoryClick(3));
+        beauty.setOnClickListener(view -> categoryClick(4));
+        book.setOnClickListener(view -> categoryClick(5));
+        internet.setOnClickListener(view -> categoryClick(6));
+        culture.setOnClickListener(view -> categoryClick(7));
     }
 
     protected void categoryClick(int i){
-        switch (i){
-            case 0:
-                intent.putExtra("category",0);
-                break;
-            case 1:
-                intent.putExtra("category",1);
-                break;
-            case 2:
-                intent.putExtra("category",2);
-                break;
-            case 3:
-                intent.putExtra("category",3);
-                break;
-            case 4:
-                intent.putExtra("category",4);
-                break;
-            case 5:
-                intent.putExtra("category",5);
-                break;
-            case 6:
-                intent.putExtra("category",6);
-                break;
-            default:
-                intent.putExtra("category",7);
-                break;
-        }
+        intent.putExtra("category",i);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/dku_dow_dpp/MycouponActivity.java
+++ app/src/main/java/com/example/dku_dow_dpp/MycouponActivity.java
@@ -1,11 +1,23 @@
 package com.example.dku_dow_dpp;
 
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Map;
+import java.util.Objects;
 
 public class MycouponActivity extends AppCompatActivity {
 
@@ -16,16 +28,70 @@ public class MycouponActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mycoupon);
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        Intent intentMyCouponDetail = new Intent(MycouponActivity.this,MyCouponDetailActivity.class);
+
+        CollectionReference colref = db.collection("user").document("lim").collection("coupon");
+        colref.get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()){
+                int i = 0;
+                TableLayout tableLayout = findViewById(R.id.myCouponListTableLayout);
+                TableRow newTableRow = new TableRow(getBaseContext());
+                for(QueryDocumentSnapshot document:task.getResult()){
+                    Map<String, Object> couponData = document.getData();
+                    View myCouponListView = LayoutInflater.from(getBaseContext()).inflate(R.layout.my_coupon_list_layout,null);
+                    Button theCoupon = myCouponListView.findViewById(R.id.myCoupon1);
+                    int finalI = i;
+                    theCoupon.setOnClickListener(view -> couponClick(intentMyCouponDetail,finalI));
+                    String stringData = Objects.requireNonNull(couponData.get("brand")) +"\n"+ Objects.requireNonNull(couponData.get("name"));
+                    theCoupon.setText(stringData);
+                    if (i==0) {
+                        TableRow tableRow = findViewById(R.id.tableRow);
+                        tableRow.addView(myCouponListView);
+                    } else if(i%2==1) {
+                        newTableRow = new TableRow(getBaseContext());
+                        newTableRow.addView(myCouponListView);
+                    } else {
+                        newTableRow.addView(myCouponListView);
+                        tableLayout.addView(newTableRow);
+                    }
+                    i++;
+                    Log.d("@@@TAG@@@", document.getId()+"=>"+document.getData());
+                }
+            } else {
+                Log.d("@@@TAG@@@", "Error getting documents: ", task.getException());
+            }
+        });
 
         buyCoupon = findViewById(R.id.buyCouponButton);
         buyCoupon.setOnClickListener(view -> {
-            Intent intent = new Intent(MycouponActivity.this,BuyCouponActivity.class);
-            startActivity(intent);
+            Intent intentBuyCoupon = new Intent(MycouponActivity.this,BuyCouponActivity.class);
+            startActivity(intentBuyCoupon);
+            finish();
         });
 
         backBtn = findViewById(R.id.backButton);
         backBtn.setOnClickListener(view -> onBackPressed());
     }
 
+    protected void couponClick(Intent intent, int i){
+        intent.putExtra("couponNum",i);
+        startActivity(intent);
+        finish();
+    }
 
 }
+//        myCoupon = findViewById(R.id.myCoupon0);
+//        Thread thread = new Thread() {
+//            public void run() {
+//                try {
+//                    Thread.sleep(1000);
+//                    runOnUiThread(() -> {
+//
+//                    });
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        };
+//        thread.start();
